0dec1f7a60b28553771826a97f2aa1aa0ddf84d8
Olaide Ojewale <ojewaleolaide@gmail.com>
2016-04-20 00:56:50 +0100

    Modify file upload to check if user identity has been verified.

--- app/controllers/media_contents_controller.rb
+++ app/controllers/media_contents_controller.rb
@@ -1,15 +1,20 @@
 class MediaContentsController < ApplicationController
 
   def create
-    @contact_file = media_params[:attachment].original_filename
-    @file_ext = @contact_file.split(".").last.downcase
-    if valid_ext?(@file_ext)
-      path = Rails.root.join("public", "uploads", @contact_file)
-      temp_save(path)
-      @session = GoogleDrive.saved_session("config.json")
-      save_to_drive(path)
+    if current_user
+      @contact_file = media_params[:attachment].original_filename
+      @file_ext = @contact_file.split(".").last.downcase
+      if valid_ext?(@file_ext)
+        path = Rails.root.join("public", "uploads", @contact_file)
+        temp_save(path)
+        @session = GoogleDrive.saved_session("config.json")
+        save_to_drive(path)
+      else
+        flash[:error] = "Invalid file type. Upload your LinkedIn .csv or .vcf file"
+        redirect_to root_url
+      end
     else
-      flash[:error] = "Invalid file type. Upload your LinkedIn .csv or .vcf file"
+      flash[:error] = "You must confirm your identity first."
       redirect_to root_url
     end
   end
@@ -41,7 +46,7 @@ private
   def save_to_drive(path)
     if @session.upload_from_file("#{path}", "#{filename}", convert: false)
       File.delete(path)
-      redirect_to user_path(session[:user_id]) if current_user.medias.create!(file_name: filename)
+      redirect_to user_path(current_user.id) if current_user.medias.create!(file_name: filename)
     else
       flash[:error] = "An error occurred. Please, try again."
       redirect_to root_url

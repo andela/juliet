9199478f4030f458458fa6773940bbbb40acda54
Olaide Ojewale <ojewaleolaide@gmail.com>
2016-04-25 23:53:23 +0100

    Add checks and validations to users and media.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  # before_action :reset_current_user_session
 
   def current_user
     User.find_by_id(session[:current_user_id])
   end
 
-  # def reset_current_user_session
-  #   session[:current_user_id] = nil
-  # end
-
-  # def is_verified?
-  #   unless current_user
-  #     flash[:error] = "You must confirm your identity first."
-  #     redirect_to root_url
-  #   end
-  # end
+  def is_verified?
+    unless current_user
+      flash[:error] = "You must confirm your identity first."
+      redirect_to root_url
+    end
+  end
 end

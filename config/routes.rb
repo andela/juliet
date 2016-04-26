45e752d3a836c8e4fd17bd1d1f3ea8af5bd16a6f
Paul Dariye <paul.dariye@gmail.com>
2016-04-26 00:51:01 -0400

    mask uri pattern

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "users#new"
-  get '/thank-you', :to => 'users#show'
-  resources :users, only: [:show, :create]
+  # get '/users/:id', to: 'users#show', as: :thank_you
+  resources :users, only: [:create]
+  resources :users, :path => '/thank_you', only: [:show], as: :thank_you
   resources :media_contents, only: [:create]
 end

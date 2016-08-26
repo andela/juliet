d282d9081a3c99080e5bc7c0bbced5d3bbfbf64b
Christopher Vundi <christophervundi@gmail.com>
2016-08-26 22:01:58 +0300

    Add keys

---
+++ keys.rb
@@ -0,0 +1,18 @@
+ENV["GH_SHEET_ID"] = "15lCiHzbfqjA-oY45sf3fuh3mEEP4pTaLAqUw8vHA4jE"
+ENV["WWR_SHEET_ID"] = "10pZKcKlq2bHy4o-Qb2WXqCBwjbzzGVEsV6Bdeq2bduo"
+ENV["WWR_URL"] = "https://weworkremotely.com/categories/2-programming/jobs"
+
+# GoogleCustomSearchApi::GOOGLE_API_KEY = "AIzaSyAa3FgaIlXHtJuGxlR3DUgMxjCoRa5mAz4"
+# GoogleCustomSearchApi::GOOGLE_API_KEY = "AIzaSyDDzhd7lRDnhjPOVyxSNmVXF2654G451pE"
+# GoogleCustomSearchApi::GOOGLE_API_KEY = "AIzaSyC61_cb5U9OwHMjc31ateh5-mdxXqpLk8Y"
+GoogleCustomSearchApi::GOOGLE_API_KEY = "AIzaSyCiKBT6cQl_dVkAQTHzZ-Imitg7cv-hH3M"
+# GoogleCustomSearchApi::GOOGLE_API_KEY = "AIzaSyBsoGLdxIsIMgPSZzLkRiqb8RqcWBKb4ko"
+# GoogleCustomSearchApi::GOOGLE_API_KEY = "AIzaSyAqtIxLX12WgfM830WK04rsoPF_-mBhGmU"
+# GoogleCustomSearchApi::GOOGLE_API_KEY = "AIzaSyD81QBaV7b4gyOmRrv3iWs2ya4a08Ls9KY"
+# GoogleCustomSearchApi::GOOGLE_API_KEY = "AIzaSyASpidxuCOmPwjeaYGKgunbVchJCbdSbTk"
+# GoogleCustomSearchApi::GOOGLE_API_KEY = "AIzaSyA-PKLda3V_7tL9GIsOXqXNAV_MQEvOZ6U"
+# GoogleCustomSearchApi::GOOGLE_API_KEY = "AIzaSyAJISY6ZiZ2aJOOlfKHbe0YyVcN16hjXM8"
+
+GoogleCustomSearchApi::GOOGLE_SEARCH_CX =  "016010277763527429812:tqprubgay5k"
+
+# GoogleCustomSearchApi::GOOGLE_SEARCH_CX =  "016010277763527429812:pznd3krs72i"
